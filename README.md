9791c23520ef3daa2a944ceb1fd957772cb1632f
huyhaq <119866766+huyhaq@users.noreply.github.com>
2023-04-14 19:42:51 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js_variable_test
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-ao3ovz)
---
+++ script.js
@@ -0,0 +1,6 @@
+let year = prompt('year');
+let month = prompt('month');
+let day = prompt('day');
+
+let message = `ngay:${day}-${month}-${year}`;
+document.write(message);
